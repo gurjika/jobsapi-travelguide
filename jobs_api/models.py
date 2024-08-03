1a9b69d165629fae57f8a7dc053f26fc4c3ced80
Luka <lgurjidze@gmail.com>
2024-08-03 18:06:40 +0400

    create job model

--- jobs_api/models.py
+++ jobs_api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+
+class Job(models.Model):
+    EMPLOYMENT_TYPE_CHOICES = [
+        ('full_time', 'Full Time'),
+        ('part_time', 'Part Time'),
+        ('contract', 'Contract'),
+        ('temporary', 'Temporary'),
+        ('internship', 'Internship'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    employment_type = models.CharField(max_length=20, choices=EMPLOYMENT_TYPE_CHOICES)
+    posted_date = models.DateField()
+    application_deadline = models.DateField()
+    salary = models.CharField(max_length=255, blank=True, null=True)
+    requirements = models.TextField()
+    responsibilities = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
