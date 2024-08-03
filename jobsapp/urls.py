e900897668607daa48e802c8b277d62eea6f9b0a
Luka <lgurjidze@gmail.com>
2024-08-03 18:49:57 +0400

    configure swagger

--- jobsapp/urls.py
+++ jobsapp/urls.py
@@ -17,9 +17,37 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+
+from django.contrib import admin
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from django.urls import include, path
+from rest_framework.permissions import AllowAny
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(AllowAny, )
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('api/', include('jobs_api.urls'))
+]
+
+
+urlpatterns += [
+   path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
